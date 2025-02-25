526318e51a6b4fd3e76c1d85e286e28ea1264353
Rendra Eka Permana <126854161+rendraekapermana@users.noreply.github.com>
2025-02-25 07:43:45 +0700

    feat: connect to MongoDB and install Mongoose & dotenv

--- server/server.js
+++ server/server.js
@@ -1,16 +1,37 @@
-const express = require('express');
-const app = express();
-const cors = require('cors');
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import cors from "cors";
 const corsOptions = {
-    origin: 'http://localhost:5173',
-}
+  origin: "http://localhost:5173",
+};
+const app = express();
+dotenv.config();
+
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO);
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    throw error;
+  }
+};
+
+mongoose.connection.on("disconnected", () => {
+  console.log("Disconnected from MongoDB");
+});
+
+mongoose.connection.on("connected", () => {
+  console.log("Connected to MongoDB");
+});
 
 app.use(cors(corsOptions));
 
-app.get('/api', (req, res) => {
-    res.json({fruits: ["apple", "banana", "orange"]});
+app.get("/api", (req, res) => {
+  res.json({ fruits: ["apple", "banana", "orange"] });
 });
 
 app.listen(8080, () => {
-    console.log("Server started on port 8080");
-})
+  connect();
+  console.log("Connected to backend with server on port 8080");
+});
